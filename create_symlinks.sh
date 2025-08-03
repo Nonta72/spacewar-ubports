67d71bf08a8a7dd0b63e468dbb6c84a2004b5370
Nonta72 <coulibalynonta@gmail.com>
2025-08-03 21:12:11 +0100

    Create create_symlinks.sh

---
+++ create_symlinks.sh
@@ -0,0 +1,5 @@
+sudo ln -s /dev/null overlay/system/etc/systemd/system/serial-getty@hvc0.service
+sudo ln -s /dev/null overlay/system/etc/systemd/user/graphical-session.target.wants/umtprd-manager.service
+sudo ln -s /dev/null overlay/system/etc/systemd/user/mtp-server-usb-moded-watcher.service
+sudo ln -s /dev/null overlay/system/usr/share/halium-overlay/vendor/lib64/hw/gralloc.common.so
+sudo ln -s /dev/null overlay/system/usr/share/halium-overlay/vendor/lib64/hw/gralloc.default.so
