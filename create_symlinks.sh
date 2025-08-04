9faadc36846ffe6071cb225976b66badd10e76c6
Coulibaly Nonta <coulibalynonta@gmail.com>
2025-08-05 00:18:01 +0100

    Fixes build

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,5 +1,8 @@
-sudo ln -s /dev/null overlay/system/etc/systemd/system/serial-getty@hvc0.service
-sudo ln -s /usr/lib/systemd/user/umtprd-manager.service overlay/system/etc/systemd/user/graphical-session.target.wants/umtprd-manager.service
-sudo ln -s /dev/null overlay/system/etc/systemd/user/mtp-server-usb-moded-watcher.service
-sudo ln -s /dev/null overlay/system/usr/share/halium-overlay/vendor/lib64/hw/gralloc.common.so
-sudo ln -s /dev/null overlay/system/usr/share/halium-overlay/vendor/lib64/hw/gralloc.default.so
+mkdir overlay/system/etc/systemd && mkdir overlay/system/etc/systemd/system
+mkdir overlay/system/etc/systemd/user && mkdir overlay/system/etc/systemd/user/graphical-session.target.wants
+mkdir overlay/system/usr/share/halium-overlay/vendor/lib64 && mkdir overlay/system/usr/share/halium-overlay/vendor/lib64/hw
+ln -s /dev/null overlay/system/etc/systemd/system/serial-getty@hvc0.service
+ln -s /dev/null overlay/system/etc/systemd/user/graphical-session.target.wants/umtprd-manager.service
+ln -s /dev/null overlay/system/etc/systemd/user/mtp-server-usb-moded-watcher.service
+ln -s /dev/null overlay/system/usr/share/halium-overlay/vendor/lib64/hw/gralloc.common.so
+ln -s /dev/null overlay/system/usr/share/halium-overlay/vendor/lib64/hw/gralloc.default.so
