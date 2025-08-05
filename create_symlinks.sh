c86af72ec5441240a57cc0eca71a009ebaad00dc
Coulibaly Nonta <coulibalynonta@gmail.com>
2025-08-05 09:40:30 +0100

    Fix symlink for umtprd

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -2,7 +2,7 @@ mkdir overlay/system/etc/systemd && mkdir overlay/system/etc/systemd/system
 mkdir overlay/system/etc/systemd/user && mkdir overlay/system/etc/systemd/user/graphical-session.target.wants
 mkdir overlay/system/usr/share/halium-overlay/vendor/lib64 && mkdir overlay/system/usr/share/halium-overlay/vendor/lib64/hw
 ln -s /dev/null overlay/system/etc/systemd/system/serial-getty@hvc0.service
-ln -s /dev/null overlay/system/etc/systemd/user/graphical-session.target.wants/umtprd-manager.service
+ln -s /usr/lib/systemd/user/umtprd-manager.service overlay/system/etc/systemd/user/graphical-session.target.wants/umtprd-manager.service
 ln -s /dev/null overlay/system/etc/systemd/user/mtp-server-usb-moded-watcher.service
 ln -s /dev/null overlay/system/usr/share/halium-overlay/vendor/lib64/hw/gralloc.common.so
 ln -s /dev/null overlay/system/usr/share/halium-overlay/vendor/lib64/hw/gralloc.default.so
