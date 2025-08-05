8bd740536cbfefed5a17dceb42e389f8c26b8cf0
Coulibaly Nonta <coulibalynonta@gmail.com>
2025-08-05 20:25:39 +0100

    Stop using tmp as BUILD_DIR

--- build.sh
+++ build.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 set -xe
+shopt -s extglob
 
-[ -d build ] || git clone https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools -b main build
-./build/build.sh "$@"
+BUILD_DIR=workdir
+
+# From https://stackoverflow.com/a/48808214
+args=("$@")
+for ((i=0; i<"${#args[@]}"; ++i)); do
+    case ${args[i]} in
+        -b) BUILD_DIR=${args[i+1]}; unset args[i]; unset args[i+1]; break;;
+    esac
+done
+
+[ -d build ] || git clone https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools build
+
+HERE=$(pwd)
+SCRIPT="$(dirname "$(realpath "$0")")"/build
+if [ ! -d "$SCRIPT" ]; then
+    SCRIPT="$(dirname "$SCRIPT")"
+fi
+TMPDOWN="$BUILD_DIR/downloads"
+mkdir -p "$TMPDOWN"
+
+source deviceinfo
+source "$SCRIPT/common_functions.sh"
+source "$SCRIPT/setup_repositories.sh" "${TMPDOWN}"
+
+# Generate lahaina_ALLYES_GKI.config from lahaina_GKI.config
+./scripts/gki/fragment_allyesconfig.sh arch/arm64/configs/vendor/lahaina_GKI.config arch/arm64/configs/vendor/lahaina_ALLYES_GKI.config
+
+cd "$HERE"
+
+./build/build.sh "${args[@]}" -b "$BUILD_DIR"
