6a5df7f4fcee181d2640456d4d1edc7ec5b69460
Coulibaly Nonta <coulibalynonta@gmail.com>
2025-08-10 17:27:32 +0100

    Clone DTS from stock NothingOS OOS

--- build.sh
+++ build.sh
@@ -32,6 +32,9 @@ echo $KERNEL_DIR
 
 cd "$TMPDOWN/$KERNEL_DIR"
 
+# Clone kernel subfolder repositories
+[ -d arch/arm64/boot/dts ] || git clone https://github.com/NothingOSS/android_kernel_devicetree_nothing_sm7325 -b sm7325/t vendor
+
 # Generate lahaina_ALLYES_GKI.config from lahaina_GKI.config
 ./scripts/gki/fragment_allyesconfig.sh arch/arm64/configs/vendor/lahaina_GKI.config arch/arm64/configs/vendor/lahaina_ALLYES_GKI.config
 
