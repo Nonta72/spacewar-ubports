8f8947363035a781c9698957a2f462ac74ef116d
Coulibaly Nonta <coulibalynonta@gmail.com>
2025-08-10 21:52:03 +0100

    That'll have to do for now

--- build.sh
+++ build.sh
@@ -35,7 +35,7 @@ cd "$TMPDOWN/$KERNEL_DIR/arch/arm64/boot/dts"
 # Clone kernel subfolder repositories
 git clone https://github.com/NothingOSS/android_kernel_devicetree_nothing_sm7325 -b sm7325/t vendor
 
-cd "$TMPDOWN/$KERNEL_DIR"
+cd ../../../../
 
 # Generate lahaina_ALLYES_GKI.config from lahaina_GKI.config
 ./scripts/gki/fragment_allyesconfig.sh arch/arm64/configs/vendor/lahaina_GKI.config arch/arm64/configs/vendor/lahaina_ALLYES_GKI.config
