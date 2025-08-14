fba103542d0a615c5ec0fc7b963feef8b67bb3dc
Coulibaly Nonta <coulibalynonta@gmail.com>
2025-08-15 00:40:02 +0100

    Ignore generic qgki config
    We have a complete defconfig now

--- build.sh
+++ build.sh
@@ -41,7 +41,7 @@ ln -s ../../../drivers/pinctrl/core.h core.h
 cd ../../../
 
 # Generate lahaina_ALLYES_GKI.config from lahaina_GKI.config
-./scripts/gki/fragment_allyesconfig.sh arch/arm64/configs/vendor/lahaina_GKI.config arch/arm64/configs/vendor/lahaina_ALLYES_GKI.config
+#./scripts/gki/fragment_allyesconfig.sh arch/arm64/configs/vendor/lahaina_GKI.config arch/arm64/configs/vendor/lahaina_ALLYES_GKI.config
 
 cd "$HERE"
 
