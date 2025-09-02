d9507c9d09243c15cd49364bae6e812c9aa7c6c0
Coulibaly Nonta <coulibalynonta@gmail.com>
2025-09-02 19:08:30 +0100

    Cleanup build script

--- build.sh
+++ build.sh
@@ -14,9 +14,6 @@ done
 
 [ -d build ] || git clone https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools build
 
-# Grab full fp5 kernel source from halium gitlab
-git clone https://gitlab.com/ubports/porting/reference-device-ports/android11/fairphone-5/kernel-fairphone-qcm6490.git -b halium-11.0-rebase $HOME/fp5
-
 HERE=$(pwd)
 SCRIPT="$(dirname "$(realpath "$0")")"/build
 if [ ! -d "$SCRIPT" ]; then
@@ -43,29 +40,10 @@ cd ../../../../techpack/audio/soc && rm -rf core.h && rm -rf pinctrl-utils.h
 ln -s ../../../drivers/pinctrl/pinctrl-utils.h pinctrl-utils.h
 ln -s ../../../drivers/pinctrl/core.h core.h
 
-# Use QCA_CLD3 and BT Stack backport from fp5 halium (too lazy to cherry-pick every single commit and yes, won't probably work)
-
-# This assumes you cloned THIS repo into $HOME/halium/spacewar-ubports
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PROJECT_ROOT="$HOME/halium/spacewar-ubports"
-WLAN_DRV_DIR="$HOME/fp5/drivers/staging"
-DEST_DIR="$PROJECT_ROOT/workdir/downloads/$KERNEL_DIR/drivers/staging"
-BT_DRV_DIR="$HOME/fp5"
-DEST_DIR_BT="$PROJECT_ROOT/workdir/downloads/$KERNEL_DIR"
-
-# Copy QCA_CLD3 files
-cp -R "$WLAN_DRV_DIR/fw-api" "$DEST_DIR"
-cp -R "$WLAN_DRV_DIR/qca-wifi-host-cmn" "$DEST_DIR"
-cp -R "$WLAN_DRV_DIR/qcacld-3.0" "$DEST_DIR"
-
-# Copy BT Stack backport files
-cp -R "$BT_DRV_DIR/backports" "$DEST_DIR_BT"
-
 # Generate lahaina_ALLYES_GKI.config from lahaina_GKI.config
 #./scripts/gki/fragment_allyesconfig.sh arch/arm64/configs/vendor/lahaina_GKI.config arch/arm64/configs/vendor/lahaina_ALLYES_GKI.config
 
 # Launch the build script !
-
 cd "$HERE"
 
 ./build/build.sh "${args[@]}" -b "$BUILD_DIR"
