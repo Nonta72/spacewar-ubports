63024b630ffcd4467e4db2262baefa1e48e20f75
Coulibaly Nonta <coulibalynonta@gmail.com>
2025-09-06 10:29:27 +0100

    Bring back the ALLYES_GKI fragment config on build time

--- build.sh
+++ build.sh
@@ -48,7 +48,7 @@ ln -s ../../../drivers/pinctrl/pinctrl-utils.h pinctrl-utils.h
 ln -s ../../../drivers/pinctrl/core.h core.h
 
 # Generate lahaina_ALLYES_GKI.config from lahaina_GKI.config
-#./scripts/gki/fragment_allyesconfig.sh arch/arm64/configs/vendor/lahaina_GKI.config arch/arm64/configs/vendor/lahaina_ALLYES_GKI.config
+./scripts/gki/fragment_allyesconfig.sh arch/arm64/configs/vendor/lahaina_GKI.config arch/arm64/configs/vendor/lahaina_ALLYES_GKI.config
 
 # Launch the build script !
 cd "$HERE"
